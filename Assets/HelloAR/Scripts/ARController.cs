0204208689f90c7cb6ec780822469533e660ae25
Maicon <maiconwz96@gmail.com>
2019-08-21 19:35:23 -0300

    Initial

---
+++ Assets/HelloAR/Scripts/ARController.cs
@@ -0,0 +1,33 @@
+﻿using GoogleARCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARController : MonoBehaviour
+{
+	private List<DetectedPlane> m_NewTrackedPlanes = new List<DetectedPlane>();
+	public GameObject GridPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Session.Status != SessionStatus.Tracking)
+		{
+			return;
+		}
+
+		Session.GetTrackables<DetectedPlane>(m_NewTrackedPlanes, TrackableQueryFilter.New);
+
+		for (int i = 0; i <m_NewTrackedPlanes.Count; i++)
+		{
+			GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+
+			grid.GetComponent<GridVisualiser>().Initialize(m_NewTrackedPlanes[i]);
+		}
+    }
+}
